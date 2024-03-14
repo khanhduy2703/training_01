a3e7511ef73ee8c7372f90affd331d92b07074eb
khanhduy2703 <nkd7059@gmail.com>
2024-03-14 12:25:16 +0700

    submit first

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5 m-auto">
+    <div className="titlle text-center text-black ">
+      <h1 className=" font-bold text-5xl mb-3"> Bài tập CSS cho text </h1>
+      <p className="mb-5 font-bold text-xs ">
+       
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum adipisci
+        harum neque odio in aperiam nulla nostrum ex dolorem, rerum
+        consectetur perspiciatis esse nemo, numquam vel sunt corporis ducimus
+        libero.
+      </p>
     </div>
+    <div className="heding_1  ">
+      <h1 className=" text-black text-2xl  font-bold mb-5"> Heading 1 </h1>
+      <p className="text_1 text-base ">
+        <span className="text_important font-bold  mb-4"> Lorem ipsum </span>
+        dolor sit amet consectetur adipisicing elit. Sequi, porro. Voluptatem numquam quisquam aperiam vel aspernatur, nobis ducimus eligendi quia adipisci quas, maiores nemo magnam sunt error recusandae eius cumque porro dolorum ipsa, eos delectus modi quos minus odio. Unde ab rerum fugit magni illo libero dicta, dolor beatae assumenda.
+      </p>
+      <p className="text_2  mb-4"> Omnis provident placeat, dolorum ab eveniet expedita quod! Sed quibusdam quas omnis suscipit itaque error, quaerat rerum voluptates assumenda velit minus perferendis impedit debitis aperiam eveniet adipisci distinctio obcaecati ipsam deserunt, doloremque, quae veritatis quisquam commodi! Aperiam eos nobis cupiditate perspiciatis ipsum nostrum iste, eius illum ipsam repudiandae nihil totam?</p>
+    </div>
+    <div className="heading_2 ">
+      <h1 className=" font-bold mb-5 text-black text-3xl"> Heading 2 </h1>
+      <p className="text_1  mb-3 text-base">
+        <span className="text_important  font-bold line-through">Lorem ipsum </span>
+        , dolor sit amet consectetur adipisicing elit. Hic molestias porro aliquam! Ut harum natus repellat aspernatur 
+        iusto voluptatibus perspiciatis voluptatum cum? Nulla officiis unde temporibus amet reiciendis facilis odio rem, maiores
+         inventore soluta? Tempore veniam, voluptates quam id fugiat saepe sapiente. Numquam voluptates dolor quis commodi saepe 
+         error, omnis rem, nihil consequuntur quisquam minima deleniti consequatur maiores mollitia labore, adipisci pariatur. Ad 
+         quidem pariatur quia ducimus deserunt laboriosam odio dolorem quasi cum dicta? Veritatis delectus dolores autem expedita
+          necessitatibus provident non aspernatur, unde odio
+         aperiam minus aut nobis officia dolorum magnam nam ipsum deserunt distinctio aliquid debitis eaque? Aperiam.
+      </p>
+    </div>
+    <div className="author  text-xs float-right  text-yellow-300"> 
+          <p>
+Phan văn cường - fouder unitop </p>
+    </div>
+  </div>
   );
 }
 
